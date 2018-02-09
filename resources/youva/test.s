10e3409d3d6d2e38c93a19e70b375101ead361f8
Youva GAUDE <ygaude@student.42.fr>
2018-02-09 21:20:20 +0100

    WOUHOU

--- resources/youva/test.s
+++ resources/youva/test.s
@@ -1,60 +1,72 @@
 .name "train"
 .comment "TCHOUTCHOU"
 
+;LAUNCHER 2
+;sti r9, r4, %490   ;0b 58 09 04 01 ea
+;sti r10, r5, %489  ;0b 58 0a 05 01 e9
+;zjmp %494          ;09 01 ee
+
+;LAUNCHER 1
+;sti r7, r4, %490   ;0b 58 07 04 01 ea
+;sti r8, r5, %489   ;0b 58 08 05 01 e9
+;zjmp %494          ;09 01 ee
+
+;LAUNCHER 0
+;sti r2, r4, %490   ;0b 58 02 04 01 ea
+;sti r3, r5, %489   ;0b 58 03 05 01 e9
+;zjmp %493          ;09 01 ed
+
+;TRAIN
 ;sti r2, r4, r6     ;0b 54 02 04 06
 ;sti r3, r5, r6     ;0b 54 03 05 06
-;live %42           ;01 00 00 00 2a
+;live %42           ;01 XX XX XX XX
 
-begin:
-	st r1, 0
-	ldi %:begin, %0, r1
-	fork %:train
-loop:
-	sti r1, %:init, %1
-	sti r1, %:launch, %11
-	sti r1, %:start, %1
-	sti r1, %:live1, %1
-	sti r1, %:live2, %1
-	sti r1, %:end, %1
-	and %1, %0, r2
 start:
-	live %-1
-	fork %:start
-live1:
-	live %-1
-	fork %:start
-live2:
-	live %-1
-	fork %:start
-end:
-	live %-1
-	zjmp %:end
-train:
+	st r1, 0
+	ld %1, r2
+	st r2, -16
+	ldi %:start, %0, r1
 init:
-	live %-1
 	fork %:split
-	ld %190054916, r2    ;0b 54 02 04 190054916 ar2
-	ldi %:start, %-2, r3
-	and r3, %65535, r3        ;00 00 FF FF
+                              ;TRAIN
+	ld %190054916, r2         ;0b 54 02 04
+	ldi %:start, %-3, r3
 	or r3, %84279296, r3      ;05 06 00 00
-;	ld %84279552, r3     ;05 06 01 00 84279552  ar3
 	ld %15, r4
 	ld %18, r5
-	and %1, %2, r16
+                              ;LAUNCHER 0
+	ld %743426, r7            ;00 0b 58 02
+	ld %1476592897, r8        ;58 03 05 01
+
+	ld %743431, r9            ;00 0b 58 07
+	ld %1476920577, r10       ;58 08 05 01
+
+	ld %743433, r11           ;00 0b 58 09
+	ld %1477051649, r12       ;58 0a 05 01
+
+	and %1, %2, r6
 	zjmp %:launch
 split:
-	ld %101405699, r2    ;06 0b 54 03 101405699 br2
-	ldi %:start, %2, r3
-	and r3, %4294967040, r3        ;FF FF FF 00
-	or r3, %11, r3      ;00 00 00 0b
-;	ld %10752, r3        ;00 00 2a 0b 10752     br3
+                              ;TRAIN
+	ld %101405699, r2         ;06 0b 54 03
+	ldi %:start, %0, r3
+	or r3, %11, r3            ;00 00 00 0b
 	ld %19, r4
 	ld %22, r5
-	and %1, %2, r16
+                              ;LAUNCHER 0
+	ld %67234315, r7          ;04 01 ea 0b
+	ld %3909681645, r8        ;e9 09 01 ed
+
+	ld %67234315, r9          ;04 01 ea 0b
+	ld %3909681646, r10       ;e9 09 01 ee
+
+	ld %67234315, r11         ;04 01 ea 0b
+	ld %3909681646, r12       ;e9 09 01 ee
+
+	and %1, %2, r6
 	zjmp %:launch
 launch:
-	sti r2, r4, r6
-	sti r3, r5, r6
-	live %-1
+	sti r11, r4, %384
+	sti r12, r5, %383
+	zjmp %388
 
-; 03 70 01 00 00 0a a4 ff fb 00 00 01 0c 00 58 0b 68 01 00 55 00 01 0b 68 01 00 aa 00 0b 0b 68 01 00 27 00 01 0b 68 01 00 28 00 01 0b 68 01 00 29 00 01 0b 68 01 00 2a 00 01 06 a4 00 00 00 01 00 00 00 02 02 01 ff ff ff ff 0c ff fb 01 ff ff ff ff 0c ff f3 01 ff ff ff ff 0c ff eb 01 ff ff ff ff 09 ff fb 01 ff ff ff ff 0c 00 2d 02 90 0b 54 02 04 02 02 90 05 06 01 00 03 02 90 00 00 00 0f 04 02 90 00 00 00 12 05 06 a4 00 00 00 01 00 00 00 02 10 09 00 2d 02 90 06 0b 54 03 02 02 90 00 00 2a 00 03 02 90 00 00 00 13 04 02 90 00 00 00 16 05 06 a4 00 00 00 01 00 00 00 02 10 09 00 03 0b 54 02 04 06 0b 54 03 05 06 01 ff ff ff ff
