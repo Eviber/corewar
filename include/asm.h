007cc4b93a1147702bd7eb86f7fd8c56ef3e835f
Welsch Morgan <welschmorgan@gmail.com>
2018-02-20 11:58:40 +0100

    fixing enum asm.h

--- include/asm.h
+++ include/asm.h
@@ -1,17 +1,24 @@
 #ifndef ASM_H
 # define ASM_H
 
-enum T_TOKEN { LABEL, INST, PARAM, ATT, COMM };
+typedef enum		e_token
+{
+	LABEL,
+	INST,
+	PARAM,
+	ATT,
+	COMM
+}			t_token;
 
 /*
-** faire une la liaison entre les nodes
+** faire la liaison entre les nodes
 */
 
-typedef struct	s_elem
+typedef struct		s_elem
 {
-  char *name;
-  T_TOKEN type;
-  unsigned long pos;
-}				t_elem;
+ 	char		*name;
+	t_token		type;
+	unsigned long	pos;
+}			t_elem;
 
 #endif
