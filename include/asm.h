e1ca3958c2c160a210523938073a4518735a3a3d
Welsch Morgan <welschmorgan@gmail.com>
2018-02-20 16:13:24 +0100

    final first draft (:p) asm.h

--- include/asm.h
+++ include/asm.h
@@ -1,24 +1,38 @@
 #ifndef ASM_H
 # define ASM_H
 
-typedef enum		e_token
-{
-	LABEL,
-	INST,
-	PARAM,
-	ATT,
-	COMM
-}			t_token;
+/*
+** in addition to T_TEG / T_IND / T_DIR in op.h
+*/
+
+# define T_INS		16
+# define T_ATT		32
 
 /*
 ** faire la liaison entre les nodes
 */
 
+typedef struct		s_op
+{
+	char		*name;
+	int		nb_params;
+	int		*type;
+	char		opcode;
+	int		nb_cycles;
+	char		*desc;
+	int		has_octal;
+	int		unknown;
+}			t_op;
+
 typedef struct		s_elem
 {
  	char		*name;
-	t_token		type;
+	int		type;
 	unsigned long	pos;
+	struct s_elem	**children; // si label: insts - si inst: params
+	struct s_elem	*next; // si label: next label - sinon null
 }			t_elem;
+
+t_op			get_fct_app(void);
 
 #endif
