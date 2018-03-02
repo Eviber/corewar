a1441db4088f74c6d66759c8bcf286a5e147d886
Eviber <youv.didj@gmail.com>
2018-03-02 18:08:08 +0100

    hud

--- vm/src/visu/visu.c
+++ vm/src/visu/visu.c
@@ -6,7 +6,7 @@
 /*   By: ygaude <ygaude@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/13 22:22:52 by ygaude            #+#    #+#             */
-/*   Updated: 2018/03/01 16:36:23 by ygaude           ###   ########.fr       */
+/*   Updated: 2018/03/02 18:06:36 by ygaude           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -163,6 +163,84 @@ void				memdisp(t_winenv *env)
 	}
 }
 
+/*
+typedef struct			s_header
+{
+	int					num;
+	unsigned int		magic;
+	char				prog_name[PROG_NAME_LENGTH + 1];
+	unsigned long		prog_size;
+	char				comment[COMMENT_LENGTH + 1];
+	unsigned long		last_live;
+	unsigned long		nb_live;
+	struct s_header		*next;
+}						t_header;
+
+typedef struct			s_process
+{
+	t_header			*champ;
+	unsigned int		pc;
+	unsigned int		carry;
+	unsigned int		reg[REG_NUMBER];
+	int					cooldown;
+	unsigned long		last_live;
+	int					inst;
+	struct s_process	*next;
+}						t_process;
+
+typedef struct			s_vm
+{
+	long				dump;
+	t_header			*champion;
+	t_header			*ll_champ;
+	t_process			*process;
+	unsigned int		nb_player;
+	unsigned char		memory[MEM_SIZE];
+	long				c_todie;
+	unsigned long		c_delta;
+	unsigned long		nbr_live;
+	unsigned long		max_check;
+	unsigned long		check;
+	unsigned long		cycle;
+	unsigned long		nb_process;
+}						t_vm;
+*/
+
+SDL_Rect			hudputstr(t_winenv *env, char *str, SDL_Rect dst)
+{
+	SDL_Texture	*tex;
+
+	tex = strtotex(str, env, (SDL_Color){255, 255, 255, 255});
+	free(str);
+	SDL_QueryTexture(tex, NULL, NULL, &dst.w, &dst.h);
+	SDL_RenderCopy(env->render, tex, NULL, &dst);
+	SDL_DestroyTexture(tex);
+	dst.y += dst.h * 2;
+	return (dst);
+}
+
+void				huddisp(t_winenv *env)
+{
+	SDL_Rect	dst;
+	char		*str;
+
+	dst = (SDL_Rect){0,0,0,0};
+	SDL_SetRenderDrawColor(env->render, 10, 10, 90, SDL_ALPHA_OPAQUE);
+	cleartex(env->render, env->hudtex);
+	ft_asprintf(&str, "Cycle : %lu%c", env->vm->cycle, 0);
+	dst = hudputstr(env, str, dst);
+	ft_asprintf(&str, "Processes : %lu%c", env->vm->nb_process, 0);
+	dst = hudputstr(env, str, dst);
+	ft_asprintf(&str, "CYCLE_TO_DIE : %lu%c", env->vm->c_todie, 0);
+	dst = hudputstr(env, str, dst);
+	ft_asprintf(&str, "CYCLE_DELTA : %lu%c", env->vm->c_delta, 0);
+	dst = hudputstr(env, str, dst);
+	ft_asprintf(&str, "NBR_LIVE : %lu%c", NBR_LIVE, 0);
+	dst = hudputstr(env, str, dst);
+	ft_asprintf(&str, "MAX_CHECKS : %lu%c", MAX_CHECKS, 0);
+	dst = hudputstr(env, str, dst);
+}
+
 void				visu_update(t_winenv *env)
 {
 	SDL_Rect		rect;
@@ -223,6 +301,7 @@ int					visu(void)
 	env = getsdlenv(NULL);
 	env->ticks = SDL_GetTicks();
 	memdisp(env);
+	huddisp(env);
 	SDL_RenderPresent(env->render);
 	while (!(env->quit |= SDL_QuitRequested()) && SDL_GetTicks() < env->ticks)
 	{}
